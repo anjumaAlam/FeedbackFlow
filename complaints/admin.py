5454f1b138711ada199a1419433a9686e6bf21a2
aunjoleenusrath <aunjoleenusrath@gmail.com>
2026-04-04 22:33:59 +0600

    updated admin.py file

--- complaints/admin.py
+++ complaints/admin.py
@@ -1,3 +1,22 @@
-from django.contrib import admin
 
-# Register your models here.
+
+from django.contrib import admin
+from .models import Complaint, ComplaintUpdate
+
+
+@admin.register(Complaint)
+class ComplaintAdmin(admin.ModelAdmin):
+    list_display = ['tracking_id', 'subject', 'student', 'complaint_type', 'status', 'priority', 'assigned_to', 'submitted_at']
+    list_filter = ['complaint_type', 'status', 'priority', 'submitted_at']
+    search_fields = ['tracking_id', 'subject', 'student__email', 'student__full_name']
+    readonly_fields = ['tracking_id', 'submitted_at', 'updated_at']
+    ordering = ['-submitted_at']
+
+
+@admin.register(ComplaintUpdate)
+class ComplaintUpdateAdmin(admin.ModelAdmin):
+    list_display = ['complaint', 'updated_by', 'status_changed_to', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['complaint__tracking_id', 'updated_by__full_name']
+    readonly_fields = ['created_at']
+    ordering = ['-created_at']
